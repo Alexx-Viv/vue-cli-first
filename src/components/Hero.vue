a1cb70cb12af1430988f0ba48c2276dfdc2e64cf
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-09-03 16:40:23 +0200

    init Vue Project

---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container"><img src="../assets/img/hero.jpeg" alt="" /></div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.container img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
